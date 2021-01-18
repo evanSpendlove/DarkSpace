f89e0b97f9c5d5ba6ec355a040f4c625fe158f96
evanSpendlove <evan.spendlove@ucdconnect.ie>
2021-01-18 13:55:25 +0000

    Refactored darkspace bot to clean up code.

--- darkspace.py
+++ darkspace.py
@@ -6,20 +6,19 @@ import discord
 from dotenv import load_dotenv
 
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
 
+TOKEN = os.getenv('DISCORD_TOKEN')
 FILE_NAME = "timetable.txt"
 
-bot = commands.Bot(command_prefix="!")
 timetable = {}
+bot = commands.Bot(command_prefix="!")
 
 def readTimetable(timetable):
     with open(FILE_NAME, 'r') as f:
-        classes = [c.split(' ') for c in f.read().strip().split('\n')]
-        for c in classes:
-            day, hour = int(c[0]), int(c[1])
-            module, link = c[2], c[3]
-            timetable[(day, hour)] = (module, link)
+        classes = f.read().strip().split('\n')
+    for c in classes:
+        day, hour, module, link = c.split(',')
+        timetable[(int(day), int(hour))] = (module, link)
     return timetable
 
 def getCurrentTime():
@@ -27,27 +26,25 @@ def getCurrentTime():
     return (now.weekday(), now.hour)
 
 def getClassLink(currentTime):
+    if currentTime not in timetable:
+        return "You got no class coming up, enjoy the break!"
     module, link = timetable[currentTime]
     msg = f"You've got {module} coming up! Here's the link: {link}"
     return msg
 
 @bot.command(pass_context=True)
-async def current_class(ctx):
-    currentTime = getCurrentTime()
-    msg = getClassLink(currentTime)
-    await bot.send_message(ctx.message.channel, msg)
+async def currentClass(ctx):
+    await ctx.send(getClassLink(getCurrentTime()))
 
 @bot.command(pass_context=True)
-async def next_class(ctx):
+async def nextClass(ctx):
     day, hour = getCurrentTime()
     currentTime = (day, hour + 1)
-    msg = getClassLink(currentTime)
-    await bot.send_message(ctx.message.channel, msg)
+    await ctx.send(getClassLink(currentTime))
 
 @bot.event
 async def on_ready():
     readTimetable(timetable)
-    print(timetable)
     print(f'{bot.user} has connected to Discord!')
 
 bot.run(TOKEN)
